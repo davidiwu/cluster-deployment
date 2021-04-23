1212362f0991981927c47f22e37f1138da09bbbf
David Wu <david.wu@pixelmatic.com>
2021-04-23 16:02:01 +0800

    add git repo entity

--- git.repo.js
+++ git.repo.js
@@ -6,11 +6,22 @@ module.exports = class GitRepo {
         this.repoBranch = options.repoBranch
         this.repoUrl = options.repoUrl;
         this.repoName = options.repoName;
-
+        this.isLocal = option.local;
     }
 
     copyGitRepoCmds() {
         const cmds = [];
-        const getRepoCmd = `git clone --single-branch --branch ${this.repoBranch} --depth=1 ${this.repoUrl} ${path.join(pkg.tmp_dir)}`
+        let getRepoCmd = `git clone --single-branch --branch ${this.repoBranch} --depth=1 ${this.repoUrl} ${path.join(pkg.tmpDir, this.repoName)}`;
+        if(this.isLocal) {
+            getRepoCmd = `cp -rf ${path.resolve(this.repoUrl)} ${path.join(pkg.tmpDir, this.repoName)}`;
+        }
+
+        cmds.push(`mkdir -p ${pkg.tmpDir}`);
+        cmds.push(`cd ${pkg.tmpDir}`);
+        cmds.push(`rm -rf ${this.repoName}`);
+        cmds.push(getRepoCmd);
+        cmds.push(`cd ${this.repoName}`);
+
+        return cmds;
     }
 }
